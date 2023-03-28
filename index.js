cc3f7bdcab121f45193496b35e5ddc42d357b71e
Irvin Dinkins <irvdude44@gmail.com>
2023-03-27 22:29:44 -0400

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+   return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(adjective = "*") {
+    return function innerAdj(childAdj = "special") {
+        return `You are ${adjective}${childAdj}${adjective}!`
+    }
+}
+
+console.log(wrapAdjective("$$$")("rich"));
